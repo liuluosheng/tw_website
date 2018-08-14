d18d4b069a9470ef4f0d9b2a7a38f5d74bafd602
liuluosheng <346642458@qq.com>
2018-08-14 22:09:21 +0800

    添加项目文件。

---
+++ tw/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tw.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Presence()
+        {
+            return View();
+
+        }
+        public ActionResult Team()
+        {
+            return View();
+
+        }
+    }
+}
